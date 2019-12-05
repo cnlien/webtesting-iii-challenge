c05e9f01fce3a59dabb4ba4549a7107729e72b0c
Christopher Lien <cnlien@gmail.com>
2019-12-05 13:45:37 -0500

    Display testing MVP

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import * as rtl from 'react-testing-library';
 import 'jest-dom/extend-expect';
+import {cleanup, render} from '@testing-library/react';
+
 import Controls from './Controls';
-afterEach(rtl.cleanup);
+
+afterEach(cleanup);
 
 describe ('should match snapshot', () => {
     it('defaults to unlocked and open gate', () => {
@@ -10,3 +13,14 @@ describe ('should match snapshot', () => {
         expect(controls.baseElement).toMatchSnapshot();
     });
 });
+
+describe ('Open Gate is Disabled', () => {
+    it('Gate is Locked. Open Button is Disabled', () => {
+        const mock = jest.fn();
+        const { queryByText } = render (
+            <Controls locked={true} closed={true} toggleLocked={mock} />
+        );
+        const openButton = queryByText("Open Gate");
+        expect (openButton.disabled).toBe(true)
+    })
+})
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,6 +1,6 @@
 import React from "react";
 import * as rtl from "@testing-library/react";
-
+import 'jest-dom/extend-expect';
 import Dashboard from './Dashboard';
 
 describe ('Should Match Snapshot', () => {
