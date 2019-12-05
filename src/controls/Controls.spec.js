f767e0841c6f0b9766ebd1744d4af9c81fe8a111
Christopher Lien <cnlien@gmail.com>
2019-12-05 11:12:29 -0500

    2 snapshot tests written for Dashboard and Controls

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,12 @@
-// Test away!
+import React from 'react';
+import * as rtl from 'react-testing-library';
+import 'jest-dom/extend-expect';
+import Controls from './Controls';
+afterEach(rtl.cleanup);
+
+describe ('should match snapshot', () => {
+    it('defaults to unlocked and open gate', () => {
+        const controls = rtl.render(<Controls />);
+        expect(controls.baseElement).toMatchSnapshot();
+    });
+});
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,11 @@
-// Test away
+import React from "react";
+import * as rtl from "@testing-library/react";
+
+import Dashboard from './Dashboard';
+
+describe ('Should Match Snapshot', () => {
+    it('Renders the Dashboard', () => {
+        const dashboard = rtl.render(<Dashboard />);
+        expect(dashboard.baseElement).toMatchSnapshot();
+    });
+});
